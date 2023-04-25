215ec29c6d734540e8862bada691993d4b9b2d43
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:39:47 +0530

    Create bot_command_scope.rb

---
+++ lib/telegram/bot/types/bot_command_scope.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class BotCommandScopeChat < Base
+
+        attribute :type, Types::String.default('chat')
+
+        attribute :chat_id, Types::String
+
+      end
+
+    end
+
+  end
+
+end
