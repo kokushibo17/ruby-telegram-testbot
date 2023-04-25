c7cd83217542fde2ea6adcca48b972fd6a32bdf5
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 12:47:34 +0530

    Create chosen_inline_result.rb

---
+++ lib/telegram/bot/types/chosen_inline_result.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ChosenInlineResult < Base
+
+        attribute :result_id, Types::String
+
+        attribute :from, User
+
+        attribute? :location, Location
+
+        attribute? :inline_message_id, Types::String
+
+        attribute :query, Types::String
+
+        alias to_s query
+
+      end
+
+    end
+
+  end
+
+end
+
+
+
+
+
+
+
+
+
