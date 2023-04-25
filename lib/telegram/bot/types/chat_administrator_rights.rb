cd79bd8cf297ed66a7012e359f39a16725eb26f1
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:51:33 +0530

    Create chat_administrator_rights.rb

---
+++ lib/telegram/bot/types/chat_administrator_rights.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ChatAdministratorRights < Base
+
+        attribute :is_anonymous, Types::Bool
+
+        attribute :can_manage_chat, Types::Bool
+
+        attribute :can_delete_messages, Types::Bool
+
+        attribute :can_manage_video_chats, Types::Bool
+
+        attribute :can_restrict_members, Types::Bool
+
+        attribute :can_promote_members, Types::Bool
+
+        attribute :can_change_info, Types::Bool
+
+        attribute :can_invite_users, Types::Bool
+
+        attribute? :can_post_messages, Types::Bool
+
+        attribute? :can_edit_messages, Types::Bool
+
+        attribute? :can_pin_messages, Types::Bool
+
+        attribute? :can_manage_topics, Types::Bool
+
+      end
+
+    end
+
+  end
+
+end
