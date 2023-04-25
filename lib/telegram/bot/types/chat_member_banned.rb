8f50c2da3e5f0ec779a86a0370c6db2169582cc1
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 12:30:13 +0530

    Create chat_member_banned.rb

---
+++ lib/telegram/bot/types/chat_member_banned.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ChatMemberBanned < ChatMember
+
+        attribute :status, Types::String
+
+        attribute :user, User
+
+        attribute :until_date, Types::Integer
+
+      end
+
+    end
+
+  end
+
+end
