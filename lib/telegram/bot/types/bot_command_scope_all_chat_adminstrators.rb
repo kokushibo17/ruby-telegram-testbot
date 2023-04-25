cc0a168cc2b33a1f97fd7f1fc421d37857815c2d
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:32:53 +0530

    Create bot_command_scope_all_chat_adminstrators.rb

---
+++ lib/telegram/bot/types/bot_command_scope_all_chat_adminstrators.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class BotCommandScopeAllChatAdministrators < Base
+
+        attribute :type, Types::String.default('all_chat_administrators')
+
+      end
+
+    end
+
+  end
+
+end
