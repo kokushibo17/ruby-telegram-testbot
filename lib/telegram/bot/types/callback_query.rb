71d9fb38fcba0d5f76571d6db773fa315e072879
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:49:16 +0530

    Create callback_query.rb

---
+++ lib/telegram/bot/types/callback_query.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class CallbackQuery < Base
+
+        attribute :id, Types::String
+
+        attribute :from, User
+
+        attribute? :message, Message
+
+        attribute? :inline_message_id, Types::String
+
+        attribute :chat_instance, Types::String
+
+        attribute? :data, Types::String
+
+        attribute? :game_short_name, Types::String
+
+      end
+
+    end
+
+  end
+
+end
