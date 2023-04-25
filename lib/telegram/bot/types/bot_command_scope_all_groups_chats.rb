d90cee66ba165f822229a763364a71bc93698f9f
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:35:56 +0530

    Create bot_command_scope_all_groups_chats.rb

---
+++ lib/telegram/bot/types/bot_command_scope_all_groups_chats.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class BotCommandScopeAllGroupChats < Base
+
+        attribute :type, Types::String.default('all_group_chats')
+
+      end
+
+    end
+
+  end
+
+end
