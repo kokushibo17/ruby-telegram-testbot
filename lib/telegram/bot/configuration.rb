ba6101bfed9fea2f860896a7da47ec26a54fa628
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:20:39 +0530

    Create configuration.rb

---
+++ lib/telegram/bot/configuration.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    class Configuration
+
+      attr_accessor :adapter, :connection_open_timeout, :connection_timeout
+
+      def initialize
+
+        @adapter = Faraday.default_adapter
+
+        @connection_open_timeout = 20
+
+        @connection_timeout = 20
+
+      end
+
+    end
+
+  end
+
+end
