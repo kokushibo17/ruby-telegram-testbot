70b7fd249fd135da677c11f19b1328aa6cc30cbe
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:28:22 +0530

    Create base.rb

---
+++ lib/telegram/bot/types/base.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class Base < Dry::Struct
+
+        include Compactable
+
+        include PatternMatching
+
+        transform_keys(&:to_sym)
+
+      end
+
+    end
+
+  end
+
+end
