4212b342fe0a495176f31847a801522d977c0233
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 12:32:29 +0530

    Create chat_member_member.rb

---
+++ lib/telegram/bot/types/chat_member_member.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ChatMemberLeft < ChatMember
+
+        attribute :status, Types::String
+
+        attribute :user, User
+
+      end
+
+    end
+
+  end
+
+end
