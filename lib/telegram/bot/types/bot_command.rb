b5652137aae8ce7afc79eedfcc73f69d6e5f9c36
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:29:31 +0530

    Create bot_command.rb

---
+++ lib/telegram/bot/types/bot_command.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class BotCommand < Base
+
+        attribute :command, Types::String
+
+        attribute :description, Types::String
+
+      end
+
+    end
+
+  end
+
+end
