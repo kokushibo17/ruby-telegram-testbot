47d93f8bde57fcc7f049605906149fe3d31029f8
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:52:46 +0530

    Create chat_invite_link.rb

---
+++ lib/telegram/bot/types/chat_invite_link.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ChatInviteLink < Base
+
+        attribute :invite_link, Types::String
+
+        attribute :creator, User
+
+        attribute :creates_join_request, Types::Bool
+
+        attribute :is_primary, Types::Bool
+
+        attribute :is_revoked, Types::Bool
+
+        attribute? :name, Types::String
+
+        attribute? :expire_date, Types::Integer
+
+        attribute? :member_limit, Types::Integer
+
+        attribute? :pending_join_request_count, Types::Integer
+
+      end
+
+    end
+
+  end
+
+end
