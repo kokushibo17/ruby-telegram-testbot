f9f25f1a133d3530aa8dbf9bfc484f65aecb8db7
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:55:07 +0530

    Create chat_location.rb

---
+++ lib/telegram/bot/types/chat_location.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ChatLocation < Base
+
+        attribute :location, Location
+
+        attribute :address, Types::String
+
+      end
+
+    end
+
+  end
+
+end
