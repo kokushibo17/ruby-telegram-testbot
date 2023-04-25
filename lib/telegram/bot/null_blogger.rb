c25d8ffc8c71d5fc046a44e20f17fbd3320ca51e
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:22:46 +0530

    Create null_blogger.rb

---
+++ lib/telegram/bot/null_blogger.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    class NullLogger < Logger
+
+      def initialize(*); end # rubocop:disable Lint/MissingSuper
+
+      def add(*); end
+
+    end
+
+  end
+
+end
