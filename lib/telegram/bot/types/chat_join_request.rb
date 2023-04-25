cf8ca5e2f4bf0fb2c0808b4414ff5807d328893f
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-25 09:53:52 +0530

    Create chat_join_request.rb

---
+++ lib/telegram/bot/types/chat_join_request.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ChatJoinRequest < Base
+
+        attribute :chat, Chat
+
+        attribute :from, User
+
+        attribute :user_chat_id, Types::Integer
+
+        attribute :date, Types::Integer
+
+        attribute? :bio, Types::String
+
+        attribute? :invite_link, ChatInviteLink
+
+      end
+
+    end
+
+  end
+
+end
