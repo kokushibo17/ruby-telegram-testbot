07641921b0470becfa941c36c4d6db323780664c
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-26 15:34:19 +0530

    Create dice.rb

---
+++ lib/telegram/bot/types/dice.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class Dice < Base
+
+        attribute :emoji, Types::String
+
+        attribute :value, Types::Integer
+
+      end
+
+    end
+
+  end
+
+end
