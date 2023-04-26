0a73f34fc71444b8fb32de450d5cb5bf69d31345
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-26 15:33:25 +0530

    Create contact.rb

---
+++ lib/telegram/bot/types/contact.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class Contact < Base
+
+        attribute :phone_number, Types::String
+
+        attribute :first_name, Types::String
+
+        attribute? :last_name, Types::String
+
+        attribute? :user_id, Types::Integer
+
+        attribute? :vcard, Types::String
+
+      end
+
+    end
+
+  end
+
+end
