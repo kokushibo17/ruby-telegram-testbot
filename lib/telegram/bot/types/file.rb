82822e7bdb5a04e5547e423858abdc8eafaa7c0a
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-26 15:39:06 +0530

    Create file.rb

---
+++ lib/telegram/bot/types/file.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class File < Base
+
+        attribute :file_id, Types::String
+
+        attribute :file_unique_id, Types::String
+
+        attribute? :file_size, Types::Integer
+
+        attribute? :file_path, Types::String
+
+      end
+
+    end
+
+  end
+
+end
