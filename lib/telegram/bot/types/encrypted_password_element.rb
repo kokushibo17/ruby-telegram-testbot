19e0a3b2137c737837c0035a7a05f0db5f15d539
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-26 15:37:41 +0530

    Create encrypted_password_element.rb

---
+++ lib/telegram/bot/types/encrypted_password_element.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class EncryptedPassportElement < Base
+
+        attribute :type, Types::String
+
+        attribute? :data, Types::String
+
+        attribute? :phone_number, Types::String
+
+        attribute? :email, Types::String
+
+        attribute? :files, Types::Array.of(PassportFile)
+
+        attribute? :front_side, PassportFile
+
+        attribute? :reverse_side, PassportFile
+
+        attribute? :selfie, PassportFile
+
+        attribute? :translation, Types::Array.of(PassportFile)
+
+        attribute :hash, Types::String
+
+      end
+
+    end
+
+  end
+
+end
