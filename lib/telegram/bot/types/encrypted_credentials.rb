142186d347af2cddc064d73659d4cab37dd60d9f
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-26 15:36:24 +0530

    Create encrypted_credentials.rb

---
+++ lib/telegram/bot/types/encrypted_credentials.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class EncryptedCredentials < Base
+
+        attribute :data, Types::String
+
+        attribute :hash, Types::String
+
+        attribute :secret, Types::String
+
+      end
+
+    end
+
+  end
+
+end
