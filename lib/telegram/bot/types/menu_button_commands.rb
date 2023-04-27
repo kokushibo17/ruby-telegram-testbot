c2d38eeef82fe5ad7d021e2dbefa3f8de037f2a9
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 13:00:15 +0530

    Create menu_button_commands.rb

---
+++ lib/telegram/bot/types/menu_button_commands.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class MenuButtonCommands < Base
+
+        attribute :type, Types::String.default('commands')
+
+      end
+
+    end
+
+  end
+
+end
