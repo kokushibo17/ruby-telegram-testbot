5bb520fd04a25bd5b8df5a0c176a264583bc7a3f
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 16:59:23 +0530

    Create web_app_data.rb

---
+++ lib/telegram/bot/types/web_app_data.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class WebAppData < Base
+
+        attribute :data, Types::String
+
+        attribute :button_text, Types::String
+
+      end
+
+    end
+
+  end
+
+end
