fc62eab17b5198fc9d39ac81e48582d7626705cb
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:51:44 +0530

    Create inline_query.rb

---
+++ lib/telegram/bot/types/inline_query.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class InlineQuery < Base
+
+        attribute :id, Types::String
+
+        attribute :from, User
+
+        attribute :query, Types::String
+
+        attribute :offset, Types::String
+
+        attribute? :chat_type, Types::String
+
+        attribute? :location, Location
+
+        alias to_s query
+
+      end
+
+    end
+
+  end
+
+end
