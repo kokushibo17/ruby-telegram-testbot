d8c3e3f6b912f5a22846651e290a78737aa912fa
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:48:17 +0530

    Create inline_keyboard_button.rb

---
+++ lib/telegram/bot/types/inline_keyboard_button.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class InlineKeyboardButton < Base
+
+        attribute :text, Types::String
+
+        attribute? :url, Types::String
+
+        attribute? :callback_data, Types::String
+
+        attribute? :web_app, WebAppInfo
+
+        attribute? :login_url, LoginUrl
+
+        attribute? :switch_inline_query, Types::String
+
+        attribute? :switch_inline_query_current_chat, Types::String
+
+        attribute? :callback_game, CallbackGame
+
+        attribute? :pay, Types::Bool
+
+      end
+
+    end
+
+  end
+
+end
