cc65ea79097f654266951f827abee5422a1ae39c
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 16:47:52 +0530

    Create menu_button_default.rb

---
+++ lib/telegram/bot/types/menu_button_default.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class MenuButtonDefault < Base
+
+        attribute :type, Types::String.default('default')
+
+      end
+
+    end
+
+  end
+
+end
