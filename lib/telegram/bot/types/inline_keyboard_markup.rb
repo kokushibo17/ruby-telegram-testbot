2a4b1531be528fa2c2ffe25a764f8c8585c35f05
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:49:30 +0530

    Create inline_keyboard_markup.rb

---
+++ lib/telegram/bot/types/inline_keyboard_markup.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class InlineKeyboardMarkup < Base
+
+        attribute :inline_keyboard, Types::Array.of(Types::Array.of(InlineKeyboardButton))
+
+        def to_compact_hash
+
+          hsh = super
+
+          hsh[:inline_keyboard].map! do |arr|
+
+            arr.map do |item|
+
+              item.is_a?(InlineKeyboardButton) ? item.to_compact_hash : item
+
+            end
+
+          end
+
+          hsh
+
+        end
+
+      end
+
+    end
+
+  end
+
+end
