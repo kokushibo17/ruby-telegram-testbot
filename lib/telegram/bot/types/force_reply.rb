de1cc041c4c460e4ae0f7aa649104ba1622f562c
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:31:50 +0530

    Create force_reply.rb

---
+++ lib/telegram/bot/types/force_reply.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class ForceReply < Base
+
+        attribute :force_reply, Types::Bool
+
+        attribute? :input_field_placeholder, Types::String
+
+        attribute? :selective, Types::Bool.default(false)
+
+      end
+
+    end
+
+  end
+
+end
