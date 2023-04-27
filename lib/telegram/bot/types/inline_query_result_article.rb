987317650efa3ec84ec14f329cb110ab940561b6
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:53:43 +0530

    Create inline_query_result_article.rb

---
+++ lib/telegram/bot/types/inline_query_result_article.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class InlineQueryResultArticle < Base
+
+        attribute :type, Types::String.default('article')
+
+        attribute :id, Types::String
+
+        attribute :title, Types::String
+
+        attribute :input_message_content, InputMessageContent
+
+        attribute? :reply_markup, InlineKeyboardMarkup
+
+        attribute? :url, Types::String
+
+        attribute? :hide_url, Types::Bool
+
+        attribute? :description, Types::String
+
+        attribute? :thumb_url, Types::String
+
+        attribute? :thumb_width, Types::Integer
+
+        attribute? :thumb_height, Types::Integer
+
+      end
+
+    end
+
+  end
+
+end
