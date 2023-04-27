f0c55eb3d203b74318dcb7dd4f2034426136e2e8
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:42:38 +0530

    Create game_high_score.rb

---
+++ lib/telegram/bot/types/game_high_score.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class GameHighScore < Base
+
+        attribute :position, Types::Integer
+
+        attribute :user, User
+
+        attribute :score, Types::Integer
+
+      end
+
+    end
+
+  end
+
+end
