395f70d790bb0bb7cf5c3bc584181b7260cfdb21
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 16:49:46 +0530

    Create sticker.rb

---
+++ lib/telegram/bot/types/sticker.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class Sticker < Base
+
+        attribute :file_id, Types::String
+
+        attribute :file_unique_id, Types::String
+
+        attribute :type, Types::String
+
+        attribute :width, Types::Integer
+
+        attribute :height, Types::Integer
+
+        attribute :is_animated, Types::Bool
+
+        attribute :is_video, Types::Bool
+
+        attribute? :thumb, PhotoSize
+
+        attribute? :emoji, Types::String
+
+        attribute? :set_name, Types::String
+
+        attribute? :premium_animation, File
+
+        attribute? :mask_position, MaskPosition
+
+        attribute? :custom_emoji_id, Types::String
+
+        attribute? :file_size, Types::Integer
+
+      end
+
+    end
+
+  end
+
+end
