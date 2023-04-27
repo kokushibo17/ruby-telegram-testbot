0a2d8ecc2fcfbb9ea8f85767f0882c2a5679ba04
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:55:16 +0530

    Create inline_query_result_audio.rb

---
+++ lib/telegram/bot/types/inline_query_result_audio.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class InlineQueryResultAudio < Base
+
+        attribute :type, Types::String.default('audio')
+
+        attribute :id, Types::String
+
+        attribute :audio_url, Types::String
+
+        attribute :title, Types::String
+
+        attribute? :caption, Types::String
+
+        attribute? :parse_mode, Types::String
+
+        attribute? :caption_entities, Types::Array.of(MessageEntity)
+
+        attribute? :performer, Types::String
+
+        attribute? :audio_duration, Types::Integer
+
+        attribute? :reply_markup, InlineKeyboardMarkup
+
+        attribute? :input_message_content, InputMessageContent
+
+      end
+
+    end
+
+  end
+
+end
