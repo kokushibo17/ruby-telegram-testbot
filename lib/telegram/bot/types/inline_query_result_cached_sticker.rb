129de0b2207d06da9ccb3645d987b41b7e0c923f
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:57:36 +0530

    Create inline_query_result_cached_sticker.rb

---
+++ lib/telegram/bot/types/inline_query_result_cached_sticker.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class InlineQueryResultCachedSticker < Base
+
+        attribute :type, Types::String.default('sticker')
+
+        attribute :id, Types::String
+
+        attribute :sticker_file_id, Types::String
+
+        attribute? :reply_markup, InlineKeyboardMarkup
+
+        attribute? :input_message_content, InputMessageContent
+
+      end
+
+    end
+
+  end
+
+end
