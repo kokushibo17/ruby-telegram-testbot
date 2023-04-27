9cc0dcf21eb745ef73f98438013f2a072d5910af
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 12:41:35 +0530

    Create game.rb

---
+++ lib/telegram/bot/types/game.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class Game < Base
+
+        attribute :title, Types::String
+
+        attribute :description, Types::String
+
+        attribute :photo, Types::Array.of(PhotoSize)
+
+        attribute? :text, Types::String
+
+        attribute? :text_entities, Types::Array.of(MessageEntity)
+
+        attribute? :animation, Animation
+
+      end
+
+    end
+
+  end
+
+end
