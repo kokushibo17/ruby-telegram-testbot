2083a9205bb5dc61406821777941e64926a229b4
ZEN X LEO <114328797+kokushibo17@users.noreply.github.com>
2023-04-27 16:54:18 +0530

    Create user_profile_photos.rb

---
+++ lib/telegram/bot/types/user_profile_photos.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Telegram
+
+  module Bot
+
+    module Types
+
+      class UserProfilePhotos < Base
+
+        attribute :total_count, Types::Integer
+
+        attribute :photos, Types::Array.of(Types::Array.of(PhotoSize))
+
+      end
+
+    end
+
+  end
+
+end
